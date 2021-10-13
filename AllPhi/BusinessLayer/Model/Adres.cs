eda370db9eb21d324910deca661352f99699e848
JarneOph <jarne.ophalvens@student.hogent.be>
2021-10-13 16:00:04 +0200

    Tests

--- AllPhi/BusinessLayer/Model/Adres.cs
+++ AllPhi/BusinessLayer/Model/Adres.cs
@@ -8,9 +8,19 @@ namespace BusinessLayer.Model
 {
     public class Adres
     {
+        public Adres(string straat, string stad, int postcode, int nummer)
+        {
+            Straat = straat;
+            Stad = stad;
+            Postcode = postcode;
+            Nummer = nummer;
+        }
+
         public string Straat { get; }
         public string Stad { get; }
         public int Postcode { get; }
         public int Nummer { get; }
+
+
     }
 }
